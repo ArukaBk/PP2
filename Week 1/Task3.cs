e0e2ab228caf821504288a94c3ebd683cfdda76e
ArukaBk <warukaw@bk.ru>
2019-06-28 20:20:37 +0600

    first commit

---
+++ Week 1/Task3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab1
+{
+    class Task3
+    {
+        public static int[] duplicateElements(int[] n)
+        {
+            int len = n.Length;
+            int[] d = new int[2 * len];
+            int cnt = 0;
+            for(int i = 0;i < len;i ++)
+            {
+                d[cnt++] = n[i];
+                d[cnt++] = n[i];
+                if (cnt >= len * 2) break;
+            }
+            return d;
+        }
+    }
+}
---
+++ Week 1/Task4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab1
+{
+    public class Task4
+    {
+
+        private static string output = "";
+
+        private static void print(string s)
+        {
+            output += s;
+        }
+        private static void println()
+        {
+            output += "\n";
+        }
+
+        public static string DrawStarTriangle(int n)
+        {
+            string s = "";
+            for(int i = 0; i < n; i++)
+            {
+                s += "[*]";
+                print(s);
+                println();
+            }
+            return output;
+        }
+    }
+}
